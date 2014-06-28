83a36a235f7e4567be3ba1b0fcd238fa4e09b338
pratik <pratik.deshpande90@gmail.com>
2014-06-28 23:30:27 +0530

    Initial Loging and Maps App.

---
+++ src/com/projectx/loginandmaps/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.projectx.loginandmaps;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        // setting default screen to login.xml
+        setContentView(R.layout.login);
+ 
+        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+ 
+        // Listening to register new account link
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View v) {
+                // Switching to Register screen
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+        
+        Button login = (Button) findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+				startActivity(i);
+			}
+		});
+	}
+	
+}
