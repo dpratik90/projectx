7f90676fdc67a33e63808b7fe6b438063c688efe
pratik <dpratik90@users.noreply.github.com>
2014-07-04 01:04:43 +0530

    Login using Foursquare and show the markers for nearby foursquare checkins

--- src/com/projectx/loginandmaps/LoginActivity.java
+++ src/com/projectx/loginandmaps/LoginActivity.java
@@ -1,42 +1,169 @@
 package com.projectx.loginandmaps;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.foursquare.android.nativeoauth.FoursquareCancelException;
+import com.foursquare.android.nativeoauth.FoursquareDenyException;
+import com.foursquare.android.nativeoauth.FoursquareInvalidRequestException;
+import com.foursquare.android.nativeoauth.FoursquareOAuth;
+import com.foursquare.android.nativeoauth.FoursquareOAuthException;
+import com.foursquare.android.nativeoauth.FoursquareUnsupportedVersionException;
+import com.foursquare.android.nativeoauth.model.AccessTokenResponse;
+import com.foursquare.android.nativeoauth.model.AuthCodeResponse;
+import com.projectx.loginandmaps.FoursquareApp.FsqAuthListener;
 
 public class LoginActivity extends Activity {
+	
+	private FoursquareApp fsapp;
+	
+	public static final String CLIENT_ID = "PDELA4BIYIQIJQ0MNGOQUJ0PPV0NPO3KLHI02SRM24HH5PCY";
+	public static final String CLIENT_SECRET = "ZMZKW3DVG535N2J0HYO4ZJNT3LRATHONLYD0HQLBCNWCFHDA";
+	
+	private static final int REQUEST_CODE_FSQ_CONNECT = 200;
+    private static final int REQUEST_CODE_FSQ_TOKEN_EXCHANGE = 201;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         // setting default screen to login.xml
         setContentView(R.layout.login);
- 
-        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
- 
-        // Listening to register new account link
-        registerScreen.setOnClickListener(new View.OnClickListener() {
- 
-            public void onClick(View v) {
-                // Switching to Register screen
-                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(i);
-            }
-        });
+        fsapp = new FoursquareApp(getApplicationContext(), CLIENT_ID, CLIENT_SECRET);
         
         Button login = (Button) findViewById(R.id.btnLogin);
         login.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent i = new Intent(getApplicationContext(), MapsActivity.class);
-				startActivity(i);
+				Intent intent = FoursquareOAuth.getConnectIntent(LoginActivity.this, CLIENT_ID);
+        		startActivityForResult(intent, REQUEST_CODE_FSQ_CONNECT);
 			}
 		});
+        FsqAuthListener listener = new FsqAuthListener() {
+        	@Override
+        	public void onSuccess() {
+        		Toast.makeText(getApplicationContext(), "Connected as " + fsapp.getUserName(), Toast.LENGTH_SHORT).show();
+//        		nameTv.setText("Connected as " + mFsqApp.getUserName());
+        	}
+        	
+        	@Override
+        	public void onFail(String error) {
+        		Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+        	}
+        };
+        
+        fsapp.setListener(listener);
 	}
+	
+	private void onCompleteConnect(int resultCode, Intent data) {
+        AuthCodeResponse codeResponse = FoursquareOAuth.getAuthCodeFromResult(resultCode, data);
+        Exception exception = codeResponse.getException();
+        
+        if (exception == null) {
+            // Success.
+            String code = codeResponse.getCode();
+            performTokenExchange(code);
+
+        } else {
+            if (exception instanceof FoursquareCancelException) {
+                // Cancel.
+                toastMessage(this, "Canceled");
+
+            } else if (exception instanceof FoursquareDenyException) {
+                // Deny.
+            	toastMessage(this, "Denied");
+                
+            } else if (exception instanceof FoursquareOAuthException) {
+                // OAuth error.
+                String errorMessage = exception.getMessage();
+                String errorCode = ((FoursquareOAuthException) exception).getErrorCode();
+                toastMessage(this, errorMessage + " [" + errorCode + "]");
+                
+            } else if (exception instanceof FoursquareUnsupportedVersionException) {
+                // Unsupported Fourquare app version on the device.
+            	toastError(this, exception);
+                
+            } else if (exception instanceof FoursquareInvalidRequestException) {
+                // Invalid request.
+            	toastError(this, exception);
+                
+            } else {
+                // Error.
+            	toastError(this, exception);
+            }
+        }
+    }
+    
+    private void onCompleteTokenExchange(int resultCode, Intent data) {
+        AccessTokenResponse tokenResponse = FoursquareOAuth.getTokenFromResult(resultCode, data);
+        Exception exception = tokenResponse.getException();
+        
+        if (exception == null) {
+            String accessToken = tokenResponse.getAccessToken();
+            // Success.
+            toastMessage(this, "Access token: " + accessToken);
+            
+            // Persist the token for later use. In this example, we save
+            // it to shared prefs.
+            //ExampleTokenStore.get().setToken(accessToken);
+            fsapp.setupAccessToken(accessToken);
+            Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+			startActivity(i);
+            
+        } else {
+            if (exception instanceof FoursquareOAuthException) {
+                // OAuth error.
+                String errorMessage = ((FoursquareOAuthException) exception).getMessage();
+                String errorCode = ((FoursquareOAuthException) exception).getErrorCode();
+                toastMessage(this, errorMessage + " [" + errorCode + "]");
+                
+            } else {
+                // Other exception type.
+            	toastError(this, exception);
+            }
+        }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_FSQ_CONNECT:
+                onCompleteConnect(resultCode, data);
+                break;
+                
+            case REQUEST_CODE_FSQ_TOKEN_EXCHANGE:
+                onCompleteTokenExchange(resultCode, data);
+                break;
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    
+    /**
+     * Exchange a code for an OAuth Token. Note that we do not recommend you
+     * do this in your app, rather do the exchange on your server. Added here
+     * for demo purposes.
+     * 
+     * @param code 
+     *          The auth code returned from the native auth flow.
+     */
+    private void performTokenExchange(String code) {
+        Intent intent = FoursquareOAuth.getTokenExchangeIntent(this, CLIENT_ID, CLIENT_SECRET, code);
+        startActivityForResult(intent, REQUEST_CODE_FSQ_TOKEN_EXCHANGE);
+    }
+    
+    public static void toastMessage(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void toastError(Context context, Throwable t) {
+        Toast.makeText(context, t.getMessage(), Toast.LENGTH_SHORT).show();
+    }
 	
 }
--- src/com/projectx/loginandmaps/MapsActivity.java
+++ src/com/projectx/loginandmaps/MapsActivity.java
@@ -1,25 +1,54 @@
 package com.projectx.loginandmaps;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends Activity {
 	// Google Map
     private GoogleMap googleMap;
+    private Intent foursquare;
+    private Location currentLocation;
+	private LocationManager locationManager;
+	private FoursquareApp fapp;
+	private ListView mListView;
+	private NearbyAdapter mAdapter;
+	private ArrayList<FsqVenue> mNearbyList;
+	private ProgressDialog mProgress;
+
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mylocation);
+        fapp 			= new FoursquareApp(getApplicationContext(), LoginActivity.CLIENT_ID, LoginActivity.CLIENT_SECRET);
+        mAdapter		= new NearbyAdapter(getApplicationContext());
+        mListView		= (ListView) findViewById(R.id.lv_places);
+        mProgress		= new ProgressDialog(this);
+        mProgress.setMessage("Loading data");
+
         try {
             // Loading map
             initilizeMap();
- 
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -47,12 +76,100 @@ public class MapsActivity extends Activity {
     protected void onResume() {
         super.onResume();
         initilizeMap();
+        setCurrentLocation();
         if (googleMap == null) {
         	Log.e("Maps", "Still not initialized");
         } else {
 	        googleMap.setMyLocationEnabled(true);
 	        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+	        loadNearbyPlaces();
         }
     }
+    
+    private void setCurrentLocation() {
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        
+        Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_COARSE);	//default
+		  
+		  
+		criteria.setCostAllowed(false); 
+		  // get the best provider depending on the criteria
+		String provider = locationManager.getBestProvider(criteria, false);
+	    
+		  // the last known location of this provider
+		currentLocation = locationManager.getLastKnownLocation(provider);
+		if (currentLocation == null) {
+			Log.e("pratik", "Current location not found!!");
+			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			startActivity(intent);
+		} else {
+			Log.e("pratik", "Hurray Found it!!");
+		}
+	}
+    
+    public static void toastMessage(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+    
+    private void loadNearbyPlaces() {
+    	mProgress.show();
+    	
+    	new Thread() {
+    		@Override
+    		public void run() {
+    			int status=0;
+    			try {
+    				mNearbyList = fapp.getNearby(currentLocation.getLatitude(), currentLocation.getLongitude());
+    			} catch (Exception e) {
+    				status=1;
+    				e.printStackTrace();
+    			}
+    			mHandler.dispatchMessage(mHandler.obtainMessage(status));
+    		}
+    	}.start();
+    }
+    
+    private Handler mHandler = new Handler() {
+    	@Override
+    	public void handleMessage(Message msg) {
+    		mProgress.dismiss();
+    		
+    		if (msg.what == 0) {
+    			if (mNearbyList.size() == 0) {
+    				Toast.makeText(getApplicationContext(), "No nearby places available", Toast.LENGTH_SHORT).show();
+    				return;
+    			}
+    			
+    			runOnUiThread(new Runnable() {
+    				@Override
+					public void run() {
+    					mAdapter.setData(mNearbyList);    			
+    	    			mListView.setAdapter(mAdapter);
+    	    			setMarkerOnMap();
+					}
+				});
+    			
+    			
+    		} else {
+    			Toast.makeText(getApplicationContext(), "Failed to load nearby places", Toast.LENGTH_SHORT).show();
+    		}
+    	}
+    };
+    
+    private void setMarkerOnMap() {
+		Iterator<FsqVenue> iter = mNearbyList.iterator();
+		while(iter.hasNext()) {
+			FsqVenue venue = iter.next();
+			Location loc = venue.location;
+			
+			double lat = loc.getLatitude();
+			double lng = loc.getLongitude();
+			LatLng ll = new LatLng(lat, lng);
+			googleMap.addMarker((new MarkerOptions()).position(ll));
+			
+		}
+		
+	}
  
 }
